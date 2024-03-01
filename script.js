4cbefbf7abfef03fa55a19740f45711301ad59ef
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-01 18:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length
+	return new Blob([str]).size
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
